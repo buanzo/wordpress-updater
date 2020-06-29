027a635dd686215344495a92bbf520ab5228b987
buanzo <buanzo@buanzo.com.ar>
2020-06-29 20:03:08 -0300

    fixes to setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_version():
 # Imports content of README.md into setuptools' long_description
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.txt'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
       long_description = f.read()
 
 
